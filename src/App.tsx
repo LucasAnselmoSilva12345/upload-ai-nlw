4591af2d75944d8b1e43d325dc09291b8f558104
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-09-12 15:27:22 -0300

    Feat: Aside component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { GithubLogo } from '@phosphor-icons/react';
+import {
+  FilmReel,
+  GithubLogo,
+  MagicWand,
+  SpinnerGap,
+} from '@phosphor-icons/react';
 import { Button } from './components/ui/button';
-import { Separator } from '@radix-ui/react-separator';
+import { Separator } from './components/ui/separator';
 import { Textarea } from './components/ui/textarea';
+import { Label } from '@radix-ui/react-label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select';
+import { Slider } from './components/ui/slider';
 
 export function App() {
   return (
@@ -43,7 +57,103 @@ export function App() {
             prompt para adicionar a descrição do vídeo selecionado
           </p>
         </section>
-        <aside className="w-80"></aside>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="cursor-pointer border flex rounded-md aspect-video border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/60"
+            >
+              <FilmReel size={16} weight="regular" />
+              Selecione um vídeo
+            </label>
+            <input
+              type="file"
+              name=""
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras chaves mencionadas no vídeo por virgulas"
+              />
+            </div>
+
+            <Button
+              type="submit"
+              className="w-full text-neutral-100 flex items-center justify-center gap-1"
+            >
+              Carregar vídeo <SpinnerGap size={16} weight="regular" />
+            </Button>
+          </form>
+
+          <Separator />
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Titulo do YouTube</SelectItem>
+                  <SelectItem value="description">
+                    Descrição do YouTube
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
+
+              <span className="block text-xs text-muted-foreground leading-relaxed">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button
+              type="submit"
+              className="w-full text-neutral-100 flex items-center justify-center gap-1"
+            >
+              Executar <MagicWand size={16} weight="regular" />
+            </Button>
+          </form>
+        </aside>
       </main>
     </div>
   );
